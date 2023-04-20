6002f6e1677c8e84ebfb1e87e8f1bc541c9c5001
Assane Bousso <boussoassane10@gmail.com>
2023-04-20 09:08:29 +0200

    Json

--- Clients.c
+++ Clients.c
@@ -2,8 +2,10 @@
 void Creation_client(char nom[50],char prenom[50]){
     FILE *fp;
     fp = fopen("ID.txt","r+");
+    fprintf(fp ,"%c %c",nom, prenom);
 }
 
 int main() {
     return 0;
+    Creation_client("Bomboclaat ","Haagrah");
 }
