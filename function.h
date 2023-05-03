ea582a24352a45342486ffc1afe89e0d0b5b6b3a
Assane Bousso <boussoassane10@gmail.com>
2023-05-03 15:43:03 +0200

    d

--- function.h
+++ function.h
@@ -2,9 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-
-void Creation_client(char nom[50],char prenom[50];
+#define MAX 1000
+void Creation_client(char nom[50],char prenom[50]);
 void ajouterproduit(char *fichier);
 void augmenterstock(char *fichier, int reference, int quant);
 void afficherstockepuise(char *fichier);
 void wordinfile(char *fichier, char *mot);
+void numinfile(char *fichier, int num);
