d06c4db91fabcc918594d6425f20573b3d1f9fe5
Assane Bousso <boussoassane10@gmail.com>
2023-05-09 15:41:54 +0200

    h

---
+++ Clients.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 1000
+void Creation_client(char nom[50],char prenom[50]);
+int ligne_corr_id(char mot[18]);
+void Supp_client_par_id(char id[18]);
