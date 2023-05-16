5de9c03deca7f9926b4b1e459d3fe144c61aec80
Assane Bousso <boussoassane10@gmail.com>
2023-05-17 00:21:42 +0200

    xcxfsfx

--- Clients.h
+++ Clients.h
@@ -4,7 +4,7 @@
 #include <stdbool.h>
 #include <time.h>
 #define MAX 1000
-void Creation_client(char nom[50],char prenom[50]);
+void creation_client(char nom[50],char prenom[50]);
 int ligne_corr_id(char mot[18]);
-void Supp_client_par_id(char id[18]);
+void supp_client_par_id(char id[18]);
 void changement_dernier(int pc, char id[18]);
