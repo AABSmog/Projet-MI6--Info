b53a5bacf4c39e882fd039a2769e43c6408dbbad
HamzaElKa <marokispots2911@gmail.com>
2023-05-22 23:18:00 +0200

    ifndef et endif

--- achat.h
+++ achat.h
@@ -1,11 +1,14 @@
-#include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<errno.h>
-void modifierstock1(char *fichier, int reference, int quant);
-void afficher_stock(char * fichier);
-void acheter(int ref, int quantite, char id[]);
-void espace_achat(char id[]);
-void connect_by_id(char *id);
-int achat(int n);
-int menu_principal();
+#ifndef ACHAT_H
+    #define ACHAT_H
+    #include <stdio.h>
+    #include<stdlib.h>
+    #include<string.h>
+    #include<errno.h>
+    void modifierstock1(char *fichier, int reference, int quant);
+    void afficher_stock(char * fichier);
+    void acheter(int ref, int quantite, char id[]);
+    void espace_achat(char id[]);
+    void connect_by_id(char *id);
+    int achat(int n);
+    int menu_principal();
+#endif
