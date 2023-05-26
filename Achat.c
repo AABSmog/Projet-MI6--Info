fcf7a0b7f2f40b2c55a580007b5a5a22b4c52c70
Assane Bousso <boussoassane10@gmail.com>
2023-05-26 19:30:08 +0200

    ccccc

--- Achat.c
+++ Achat.c
@@ -4,29 +4,30 @@
 #include <errno.h>
 #include "Clients.h"
 #include "gestion.h"
+#include "main.h"
 
 #define MAX 1000
 
 int getref(int ref)
 {
-  //zone de declaration d'un pointeur vers un fichier
+  // zone de declaration d'un pointeur vers un fichier
   FILE *file;
-  //zone de declaration de 3 tableau de char et un pointeur vers un char  
+  // zone de declaration de 3 tableau de char et un pointeur vers un char
   char line[100], name[100], file_name[100], *refh;
-  //declaration de variables entières
+  // declaration de variables entières
   int reference, stock, size, c = 0, i = 0;
-   //declaration de variable flottante 
+  // declaration de variable flottante
   float price;
-  //ouverture du fichier produit.txt en mode lecture   
+  // ouverture du fichier produit.txt en mode lecture
   file = fopen("produit.txt", "r");
-  //parcours jusqu'à la fin du fichier ou jusqu'à trouver la reference recherchée 
+  // parcours jusqu'à la fin du fichier ou jusqu'à trouver la reference recherchée
   while (fgets(line, sizeof(line), file) != NULL)
   {
-    //scan des différences references   
+    // scan des différences references
     sscanf(line, "%s %d %d %f %d", name, &reference, &stock, &price, &size);
     if (reference == ref)
     {
-      c = 1;//indiquer qu'on a trouvé la reference recherchée
+      c = 1;        // indiquer qu'on a trouvé la reference recherchée
       fclose(file); // sort de la boucle la plus proche
       return 1;
       break;
@@ -34,7 +35,7 @@ int getref(int ref)
   }
   if (c == 1)
   {
-     // si on a trouvé on ne fait rien de particulier
+    // si on a trouvé on ne fait rien de particulier
   }
   else
   {
@@ -54,14 +55,14 @@ void display_stock(char *file1)
   int reference, quantity, size;
   // Declaration de variable flotante
   float price;
-   // declaration de tableau de caractere
+  // declaration de tableau de caractere
   char name[100];
   char line[100];
-// Ouverture du fichier "produit.txt" en mode lecture
+  // Ouverture du fichier "produit.txt" en mode lecture
   file = fopen(file1, "r");
   // scan les infos de la premiere ligne
   sscanf(line, "%s %d %d %f %d ", name, &reference, &quantity, &price, &size); // verif si la fonction retourne le bon num
-  // parcours du fichier ligen par ligne 
+  // parcours du fichier ligen par ligne
   while (fscanf(file, "%s %d %d %f %d", name, &reference, &quantity, &price, &size) == 5)
   {
     if (quantity != 0)
@@ -79,39 +80,38 @@ void display_stock(char *file1)
 float buy(int ref, int quantity, char id[])
 {
   float spent = 0;
-  //decaration d'un pointeur vers un fichier
+  // decaration d'un pointeur vers un fichier
   FILE *file;
   FILE *fk;
   char line[100], name[100], file_name[100], filenameb[60], bal[30], *refh;
-  int reference, stock, size,c = 0, i = 0;
-  float price , x, y;
-   // ouverture du fichier produit.txt
+  int reference, stock, size, c = 0, i = 0;
+  float price, x, y;
+  // ouverture du fichier produit.txt
   file = fopen("produit.txt", "r");
   sprintf(filenameb, "Acc_balance/%s.txt", id); // création d'un fichier historique
-  fk = fopen(filenameb,"r+");
+  fk = fopen(filenameb, "r+");
   fgets(bal, sizeof(bal), fk);
   x = atof(bal);
-   //parcours du fichier ligne par ligne jusquà la fin ou jusqu'à ce qu'on trouve la reference correspondante
+  // parcours du fichier ligne par ligne jusquà la fin ou jusqu'à ce qu'on trouve la reference correspondante
   while (fgets(line, sizeof(line), file) != NULL)
   {
     sscanf(line, "%s %d %d %f %d", name, &reference, &stock, &price, &size);
-    y = quantity * price ;
+    y = quantity * price;
     if (reference == ref && stock - quantity >= 0 && y < x)
     {
-      // diminue le stock quand on fait un acaht 
+      // diminue le stock quand on fait un acaht
       fclose(file);
       modifystock("produit.txt", ref, -quantity);
-       // calcul des depenses
+      // calcul des depenses
       spent = spent + price * quantity;
 
       for (i = 0; i < quantity; i++)
       {
         change_last(ref, id);
-        modify_balance(y*-1,id);
-        
+        modify_balance(y * -1, id);
       }
-      c = 1;// montrer qu'on a trouvé le produit
-      break;// sort de la boucle la plus proche
+      c = 1; // montrer qu'on a trouvé le produit
+      break; // sort de la boucle la plus proche
     }
     else
     {
@@ -127,19 +127,19 @@ float buy(int ref, int quantity, char id[])
     printf("Impossible de faire cette achat.\n");
     return 0;
   }
-  //fermeture du fichier
+  // fermeture du fichier
   fclose(file);
   return spent;
 }
 
 void shopping_area(char id[])
 {
-  //declaration de variable
+  // declaration de variable
   int choice, ref, quant;
   float spent = 0.0, balr;
   char name[50], firstname[50];
   char input[MAX];
-  //ouverture d'un menu doté de la capacité de se reouvrir automatiquement quand l'utilisateur rentre un mauvais choix
+  // ouverture d'un menu doté de la capacité de se reouvrir automatiquement quand l'utilisateur rentre un mauvais choix
   do
   {
     printf("\nVous voila dans la zone achat!\n");
@@ -149,7 +149,7 @@ void shopping_area(char id[])
     printf("3.Acceder a l'historique d'achat.\n");
     printf("4.Rajouter de l'argent sur votre compte\n");
     printf("5.Quitter et ayez la possibilite de supprimer votre compte.\n");
-  
+    printf("6.Aller au menu principal.");
     printf("\nVotre choix:");
     choice = scanint(input);
     printf("\n");
@@ -161,37 +161,43 @@ void shopping_area(char id[])
     }
     else if (choice == 2)
     {
-       // demander à l'utilisateur de rentrer la reference et la quantite du produit qu'il souhaite acheter et entrer ces parametres dans la fonction buy
+      // demander à l'utilisateur de rentrer la reference et la quantite du produit qu'il souhaite acheter et entrer ces parametres dans la fonction buy
       printf("Donnez la reference du produit que vous souhaitez acheter:");
       ref = scanint(input);
-       // verifie si la ref est bonne ou pas
+      // verifie si la ref est bonne ou pas
       int i = getref(ref);
-      if(i==1){
-      printf("Donnez la quantite que vous souhaitez acheter de ce produit:");
-      quant = scanint(input);
-      // recuperation de la depense totale
-      spent = buy(ref, quant, id) + spent;
+      if (i == 1)
+      {
+        printf("Donnez la quantite que vous souhaitez acheter de ce produit:");
+        quant = scanint(input);
+        // recuperation de la depense totale
+        spent = buy(ref, quant, id) + spent;
       }
-       // affichage de la depense totale
+      // affichage de la depense totale
       printf("Vous avez de depenser: %.02f \n", spent);
     }
     else if (choice == 3)
     {
-      //acceder à l'historique
+      // acceder à l'historique
       history(id);
     }
     else if (choice == 4)
     {
       printf("Veuillez rentrer un montant à recharger.\n");
-      balr=scanfloat(input);
+      balr = scanfloat(input);
       modify_balance(balr, id);
     }
-    else if (choice == 5) {
+    else if (choice == 5)
+    {
       printf("A la prochaine!\n");
+    }
+    else if (choice == 6)
+    {
+      main_menu();
     }
     else
     {
-      printf("Erreur veuillez rentrer un nombre entre 1; 2; 3 et 4.\n");
+      printf("Erreur veuillez rentrer un nombre entre 1 et 6.\n");
       break;
     }
   } while (choice != 5);
@@ -202,54 +208,55 @@ void connect_by_id(char *id)
   FILE *file;
   char line[100], username[100], name[100], firstname[100];
   int c = 0;
-   // verification du pointeur id
+  // verification du pointeur id
   if (id == NULL)
   {
     printf("erreur:\n");
   }
-   // ouverture du fichier ID.txt
+  // ouverture du fichier ID.txt
   file = fopen("ID.txt", "r");
-   // verification du pointeur
+  // verification du pointeur
   if (file == NULL)
   {
     printf("erreur");
     exit(1);
   }
-   // parcours du fichier ligne par ligne
+  // parcours du fichier ligne par ligne
   while (fgets(line, sizeof(line), file) != NULL || c == 1)
   {
-     // scan des infos de chaque ligne
+    // scan des infos de chaque ligne
     sscanf(line, "%s %s %s", username, name, firstname);
     // comparaison de l'id rentré avce les id presents dans le fichier
     if (strcmp(id, username) == 0)
     {
       printf("Bienvenue %s %s!\n", name, firstname);
-      c = 1;// indiquer qu'on a trouvé l'id
-      break;// sortir de la boucle la plus proche
+      c = 1; // indiquer qu'on a trouvé l'id
+      break; // sortir de la boucle la plus proche
     }
   }
   if (c == 1)
   {
-     // si l'utilisateur à reussi à se connecter il rentre immediatement dans la zone d'achat 
+    // si l'utilisateur à reussi à se connecter il rentre immediatement dans la zone d'achat
     fclose(file);
     shopping_area(id);
   }
   else
   {
-     // si l'utilisateur n'a pas pu se connecter afficher id introuvable
+    // si l'utilisateur n'a pas pu se connecter afficher id introuvable
     printf("Identifiant introuvable.\n");
   }
-   // fermer le fichier 
+  // fermer le fichier
   fclose(file);
 }
 
 int buying(int n)
 {
-// declaration de variable
+  // declaration de variable
   int choice = 0;
+  float balr;
   char name[50], firstname[50], id[100];
   char input[MAX];
-// ouverture d'un menu qui a la capacité de se reouvrir dès que l'utilisateur se trompe de totalement de choix
+  // ouverture d'un menu qui a la capacité de se reouvrir dès que l'utilisateur se trompe de totalement de choix
   do
   {
     printf("\nMode achat:\n");
@@ -257,7 +264,9 @@ int buying(int n)
     printf("1.Creer un compte.\n");
     printf("2.Vous connecter.\n");
     printf("3.Supprimer votre compte.\n");
-    printf("4.Quitter le programme .\n\n");
+    printf("4.Rajouter de l'argent sur votre compte\n");
+    printf("5.Quitter le programme .\n\n");
+    printf("6.Revenir au menu principal.");
     printf("Votre choix:");
     choice = scanint(input);
     if (choice == 1)
@@ -271,31 +280,43 @@ int buying(int n)
     }
     else if (choice == 2)
     {
-       // demande à l'utilisateur de rentrer son id pour se connecter
+      // demande à l'utilisateur de rentrer son id pour se connecter
       printf("Entrez votre ID:");
       scanf("%s", id);
       connect_by_id(id);
     }
     else if (choice == 3)
     {
-       // demander à l'utilisateur son ID pour le supprimer
-       printf("Entrez vos ID:\n");
+      // demander à l'utilisateur son ID pour le supprimer
+      printf("Entrez vos ID:\n");
       printf("Entrez vos ID:\n");
       scanf("%s", id);
       suppression_id(id);
     }
-    else if (choice == 4)
+
+    else if (choice == 5)
     {
-      //message d' au revoir
+      // message d' au revoir
       printf("Merci pour votre visite !");
       exit(0);
     }
+    else if (choice == 6)
+    {
+      main_menu();
+    }
+    else if (choice == 4)
+    {
+      printf("Veuillez rentrer un montant à recharger.\n");
+      balr = scanfloat(input);
+      modify_balance(balr, id);
+    }
+
     else
     {
-      // message indiquant quel type de coordonnées on doit rentrer 
-      printf("Erreur veuillez rentrer un nombre entre 1; 2; 3 et 4.\n");
+      // message indiquant quel type de coordonnées on doit rentrer
+      printf("Erreur veuillez rentrer un nombre entre 1 et 6.\n");
       break;
     }
-  } while (choice != 4);
+  } while (choice != 5);
   exit(0);
 }
